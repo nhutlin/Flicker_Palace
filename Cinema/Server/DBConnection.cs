b229c429adc8bfef707e070873e7b27fc0672c14
Hoàng Trí Tường <110546242+1esuoK@users.noreply.github.com>
2023-05-25 14:34:38 +0700

    initiate db connection

---
+++ Cinema/Server/DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class DBConnection
+    {
+        private string sql_conn = @"Data Source=TRITUONG;Initial Catalog=FLICKERPALACE;Integrated Security=True";
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(sql_conn);
+        }
+    }
+}
